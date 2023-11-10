ebfb0cdd297b76c47f272a42a6bdc9c9893277a2
IbraAguero <ibrahimaguero@gmail.com>
2023-11-10 10:39:44 -0300

    fix import printerSchema

--- back/routes/printer.routes.js
+++ back/routes/printer.routes.js
@@ -1,25 +1,25 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
   getPrinters,
   getPrinter,
   createPrinter,
   updatePrinter,
   deletePrinter,
-} from '../controllers/printer.controller.js';
-import { validateSchema } from '../middleware/validatorSchema.js';
-import { printerSchema } from '../schemas/printer.schema.js';
-import verifyJWT from '../middleware/verifyJWT.js';
+} from "../controllers/printer.controller.js";
+import { validateSchema } from "../middleware/validatorSchema.js";
+import verifyJWT from "../middleware/verifyJWT.js";
+import { printerSchema } from "../schemas/printer.schema.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.get('/', getPrinters);
-router.get('/:id', getPrinter);
+router.get("/", getPrinters);
+router.get("/:id", getPrinter);
 
-router.post('/', validateSchema(printerSchema), createPrinter);
-router.put('/:id', validateSchema(printerSchema.partial()), updatePrinter);
-router.delete('/:id', deletePrinter);
+router.post("/", validateSchema(printerSchema), createPrinter);
+router.put("/:id", validateSchema(printerSchema.partial()), updatePrinter);
+router.delete("/:id", deletePrinter);
 //router.use('/impresoras', authRequired, extractType, makerRoutes);
 
 export default router;
